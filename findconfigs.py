6f97e10bfded5fa73736ac9df38e04dd28f0f165
Bill Hunt <bill@krues8dr.com>
2016-09-25 10:43:21 -0400

    Adding outfile option to findconfigs.

--- findconfigs.py
+++ findconfigs.py
@@ -3,28 +3,37 @@
 # Finds files named settings or config in folders and allows the user to
 # review all of them for revealed passwords. Outputs a list of flagged files.
 
+# TODO:
+
 from __future__ import print_function
 import glob2
 import sys
 import readline
-
 import pydoc
+import argparse
 
-if len(sys.argv) <2:
-  print('You must specify a directory to search.', file=sys.stderr)
-  exit()
+parser = argparse.ArgumentParser(description='Finds configuration files, and lets the user review and flag them.')
+parser.add_argument('directory', help='the directory to search. (Recursive)')
+parser.add_argument('-o', dest='outfile', type=argparse.FileType('w'), help='file to write the results to.')
 
-gitdir = sys.argv[1].strip().rstrip('/')
+args = parser.parse_args()
 
-files = glob2.glob('{}/**/*settings.*'.format(gitdir))
-files.extend(glob2.glob('{}/**/*config.*'.format(gitdir)))
+confdir = args.directory.strip().rstrip('/')
 
-flagged_files = []
+print('Searching ' + confdir)
+
+files = glob2.glob('{}/**/*settings.*'.format(confdir))
+files.extend(glob2.glob('{}/**/*config.*'.format(confdir)))
 
 print('{} config files found.'.format(str(len(files))))
 print('')
+
+if len(files) == 0:
+  exit()
+
 raw_input('Press RETURN to begin reviewing these files. Press q to exit the review tool.')
 
+flagged_files = []
 for file in files:
   print('File ', file)
   with open(file, 'r') as f:
@@ -35,14 +44,25 @@ for file in files:
     flagged_files.append(file)
 
 print('')
-print('{} config files found.'.format(str(len(flagged_files))))
-print('')
-print('-- Files --')
+print('{} config files flagged.'.format(str(len(flagged_files))))
 print('')
 
-for file in flagged_files:
-  print(file)
+if len(flagged_files) == 0:
+  exit()
 
-print('')
-print('-- End Files --')
-print('')
+if args.outfile:
+  print('Writing output to ' + args.outfile.name)
+  with args.outfile as f:
+    for file in flagged_files:
+      f.write(file + "\n")
+
+else:
+  print('-- Files --')
+  print('')
+
+  for file in flagged_files:
+    print(file)
+
+  print('')
+  print('-- End Files --')
+  print('')
